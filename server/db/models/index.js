41a0cd05526f8cdff860338f2897abb4e43da083
Cecilia Avery <cilsong@gmail.com>
2018-04-26 16:17:14 -0400

    db models set

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,4 +1,6 @@
 const User = require('./user')
+const Decision = require('./decision')
+const Point = require('./point')
 
 /**
  * If we had any associations to make, this would be a great place to put them!
@@ -13,6 +15,11 @@ const User = require('./user')
  * for example, we can say: const {User} = require('../db/models')
  * instead of: const User = require('../db/models/user')
  */
+Decision.hasMany(Point)
+Point.belongsTo(Decision)
+
 module.exports = {
-  User
+  User,
+  Decision,
+  Point
 }
